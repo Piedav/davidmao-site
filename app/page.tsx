16714936ccdd3f53903b9008456d2d65c5c9f5dd
David Mao <maodavid1012@gmail.com>
2026-04-26 17:18:55 -0400

    red only bg

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,16 @@ import { useEffect, useState } from "react";
 import type { CSSProperties } from "react";
 
 export default function Home() {
-  const [hue, setHue] = useState(20);
+  //const [hue, setHue] = useState(20);
   const [time, setTime] = useState(0);
 
-  const radius = 80;
+  const radius = 50;
 
   const x = Math.sin(time) * radius + 50;
   const y = Math.cos(time) * radius + 50;
   const x2 = Math.sin(time + Math.PI) * radius + 50;
   const y2 = Math.cos(time + Math.PI) * radius + 50;
+  const hue = 30 + Math.sin(time) * 30;
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -22,19 +23,19 @@ export default function Home() {
     return () => clearInterval(interval);
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setHue((prevHue) => (prevHue + 0.2) % 360);
-    }, 15);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setHue((prevHue) => (prevHue + 0.2) % 360);
+  //   }, 15);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   const dynamicBackground: CSSProperties = {
     background: `
-      radial-gradient(circle 60vw at ${x}% ${y}%, hsl(${hue-50}, 80%, 70%), transparent),
+      radial-gradient(circle 50vw at ${x}% ${y}%, hsl(${hue-25}, 80%, 70%), transparent),
       radial-gradient(circle 100vw at 50% 50%, hsl(${hue}, 80%, 70%), transparent),
-      radial-gradient(circle 50vw at ${x2}% ${y2}%, hsl(${hue + 50}, 80%, 70%), transparent),
+      radial-gradient(circle 50vw at ${x2}% ${y2}%, hsl(${hue + 25}, 80%, 70%), transparent),
       #ffffff
     `,
   };
@@ -62,19 +63,20 @@ const styles: Record<string, CSSProperties> = {
     textAlign: "center",
     backdropFilter: "blur(24px)",
     backgroundColor: "rgba(0, 0, 0, 0.2)",
-    padding: "2.5rem",
-    borderRadius: "1rem",
+    padding: "5rem",
+    borderRadius: "5rem",
     border: "1px solid rgba(255, 255, 255, 0.1)",
   },
 
   title: {
-    fontSize: "3rem",
+    fontSize: "8rem",
     fontWeight: 700,
     letterSpacing: "-0.025em",
   },
 
   subtitle: {
     marginTop: "1.5rem",
-    color: "rgb(244, 244, 245)",
+    color: "rgba(255, 255, 255, 0.74)",
+    fontSize: "3rem"
   },
 };
